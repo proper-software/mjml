728d3a03d8e2eb4d0a5e654fcf73843e14fbff9f
tomasz-nolberczak <tomasz.nolberczak@gmail.com>
2020-12-22 11:40:34 +0100

    Bumped version of a package into 0.1.0

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Npm.depends({
 
 Package.describe({
   name: "propersoftware:mjml",
-  version: "0.0.2",
+  version: "0.1.0",
   summary: "Use your MJML templates with meteor/emails package",
   git: "https://github.com/proper-software/mjml.git",
   documentation: "README.md",
