eb260b7592c8f8e7e1908e640c5de8495601b967
tomasz-nolberczak <tomasz.nolberczak@gmail.com>
2020-12-22 11:35:38 +0100

    Log object is now a class

--- utils/Log.class.ts
+++ utils/Log.class.ts
@@ -1,13 +1,31 @@
+import ILogParams from "../interfaces/ILogParams";
+
 const consoleModule = Npm.require("console");
 
-export const Log = {
-  error: (text: string): void => {
-    consoleModule.log("\x1b[31m", `[ERROR]\t ${text}`);
-  },
-  warning: (text: string): void => {
-    consoleModule.log("\x1b[33m", `[WARNING]\t ${text}`);
-  },
-  success: (text: string): void => {
-    consoleModule.log("\x1b[32m", `[SUCCESS]\t ${text}`);
-  },
+export class Log {
+  private outputEnabled : boolean;
+
+  constructor(params : ILogParams) {
+    this.outputEnabled = params.enableOutput;
+  }
+
+  isOutputEnabled() : boolean {
+    return this.outputEnabled === true;
+  }
+
+  error(text: string): void {
+    if (this.isOutputEnabled()) {
+      consoleModule.log("\x1b[31m", `[ERROR]\t ${text}`);
+    }
+  }
+  warning(text: string): void {
+    if (this.isOutputEnabled()) {
+      consoleModule.log("\x1b[33m", `[WARNING]\t ${text}`);
+    }
+  }
+  success(text: string): void {
+    if (this.isOutputEnabled()) {
+      consoleModule.log("\x1b[32m", `[SUCCESS]\t ${text}`);
+    }
+  }
 };
